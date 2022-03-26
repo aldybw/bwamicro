a49852dc3b9c9f7c774ad294b8e7950f5b8c674a
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-03-26 20:10:16 +0700

    setup axios

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 import Router from "next/router";
+import { ToastContainer } from "react-toastify";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
 import "../../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 // import "../tailwindcss/style.css";
 
 NProgress.configure({});
@@ -10,7 +12,12 @@ Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer position="top-center"></ToastContainer>
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Home() {
+import axios from "configs/axios";
+
+function Home(props) {
+  console.log(props);
   return (
     <div className="container mx-auto mt-4">
       <Head>
@@ -19,3 +22,14 @@ export default function Home() {
     </div>
   );
 }
+
+Home.getInitialProps = async () => {
+  try {
+    const data = await axios.get(`/courses`);
+    return { data: data.data.data };
+  } catch (error) {
+    return error;
+  }
+};
+
+export default Home;
